4d2dd694226e030ab15a31612d3b03208751e1c1
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-18 09:08:32 -0300

    chore: config prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true, 
+  env: {
+    node: true, 
+    es6: true
+  }, 
+  parserOptions: { ecmaVersion: 8 }, // enable latest ecmascript features such as async/await
+  ignorePatterns: ['node_modules/*', '.next/*', '.out/*', '!.prettierrc.js'], // we don't wanna check those files for lint problems 
+  extends: ['eslint:recommended'], 
+  overrides: [
+    // this config will apply only for js files
+    {
+      files: ['**/*.ts', '**/*.tsx'], 
+      parser: '@typescript-eslint/parser', 
+      settings: {react: { version: 'detect' }}, 
+      env: {
+        browser: true,
+        node: true,
+        es6: true,
+      }, 
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended', // TypeScript rules
+        'plugin:react/recommended', // React rules
+        'plugin:react-hooks/recommended', // React hooks rules
+        'plugin:jsx-a11y/recommended', // Accessibility rules
+        'plugin:prettier/recommended', // Prettier plugin
+      ], 
+      rules: {
+        // We will use TypeScript's types for component props instead
+        'react/prop-types': 'off',
+
+        // No need to import React when using Next.js
+        'react/react-in-jsx-scope': 'off',
+
+        // This rule is not compatible with Next.js's <Link /> components
+        'jsx-a11y/anchor-is-valid': 'off',
+
+        // Why would you want unused vars?
+        '@typescript-eslint/no-unused-vars': ['error'],
+
+        // I suggest this setting for requiring return types on functions only where useful
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            allowExpressions: true,
+            allowConciseArrowFunctionExpressionsStartingWithVoid: true,
+          },
+        ],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Includes .prettierrc.js rules
+      },
+    }
+  ]
+ }
