d69f5e6fbdb65c65a2deb30636e7279a4c6a8386
Gabriel Davi <gdavi.gd@gmail.com>
2021-05-27 22:50:58 -0300

    chore: config styles theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import { Box, Heading } from '@chakra-ui/react'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <Box as="main">
+      <Heading>Home</Heading>
+    </Box>
+  )
 }
---
+++ styles/theme.ts
@@ -0,0 +1,32 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    hightlight: {
+      main: '#FFBA08',
+    },
+    dark: {
+      black: '#000',
+      heading: '#47585B',
+      text: '#47585B',
+      info: '#999',
+    },
+    light: {
+      white: '#fff',
+      heading: '#F5F8FA',
+      info: '#DADADA',
+    },
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'light.heading',
+        color: 'dark.heading',
+      },
+    },
+  },
+})
